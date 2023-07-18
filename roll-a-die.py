fd9ef901011ca10d6579950231ae3e48089e7b2d
danube1 <107928098+danube1@users.noreply.github.com>
2023-07-18 17:16:58 +0530

    Add files via upload

---
+++ roll-a-die.py
@@ -0,0 +1,14 @@
+import random
+no=random.randint(1,6)
+if no==1:
+    print("0")
+if no==2:
+    print("00")
+if no==3:
+    print("000")
+if no==4:
+    print("0000")
+if no==5:
+    print("00000")
+if no==6:
+    print("000000")    
